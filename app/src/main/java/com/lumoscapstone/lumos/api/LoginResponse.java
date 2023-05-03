b670696bd6092ec8fd64e7327db0e16bbe9036d9
udromero <114256257+udromero@users.noreply.github.com>
2023-05-02 19:15:26 -0700

    Login Page Integration
    Integrated the backend with our login page, now sending and fetching user information to verify user login's through a post request call.

---
+++ app/src/main/java/com/lumoscapstone/lumos/api/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.lumoscapstone.lumos.api;
+
+public class LoginResponse {
+    private String token;
+    private int id;
+
+    public String getToken() {
+        return token;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
--- app/src/main/java/com/lumoscapstone/lumos/api/LumosAPI.java
+++ app/src/main/java/com/lumoscapstone/lumos/api/LumosAPI.java
@@ -3,6 +3,9 @@ package com.lumoscapstone.lumos.api;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
@@ -11,14 +14,23 @@ public interface LumosAPI {
     // Notes:
     // Only enter the relative url, base URL will be passed in later
     // Relative url part as an example: https://baseUrlForOurAPI.com/relative_url
+    // Example URL: https://lumos.benjaminwoodward.dev/api/item/1
     // RetroFit will auto-generate the code for these methods
+
+    // Get user information via id
+    @GET("api/user/{id}")
+    Call<User> getUser(@Path("id") int userId);
+
+    // Verify user log in + grab user id
+    @POST("api/login")
+    Call<LoginResponse> verifyLogin(@Body LoginRequest loginRequest);
 
     // Get list of items based on distance
     // TODO: add getter method which returns a list of items based on a max distance (may need to modify Item.java's member variables)
 
-    // Get item details by item ID
-    @GET("api/item/{id}")
-    Call<Item> getItem(@Path("id") int itemId);
+    // Get item details by item ID (not sure if correct)
+//    @GET("api/item/{id}")
+//    Call<Item> getItem(@Path("id") int itemId);
 
     // Attempt to reserve an item
     // TODO: add definition of this POST method
