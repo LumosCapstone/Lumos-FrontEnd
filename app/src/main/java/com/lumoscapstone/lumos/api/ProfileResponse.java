78d83c6d6fd8342c757465d0fbb243ee446025c1
JustinThon <jthon@csumb.edu>
2023-05-18 13:28:18 -0700

    New updated files

--- app/src/main/java/com/lumoscapstone/lumos/api/ProfileResponse.java
+++ app/src/main/java/com/lumoscapstone/lumos/api/ProfileResponse.java
@@ -1,2 +1,14 @@
-package com.lumoscapstone.lumos.api;public class ProfileResponse {
+package com.lumoscapstone.lumos.api;
+
+public class ProfileResponse {
+    private String message;
+    private int Id;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getId() {
+        return Id;
+    }
 }
